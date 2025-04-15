ae0fb791ef6212198b7c59d41e43829d1edabc12
KeifSant <keyllafsantos1@gmail.com>
2025-04-15 12:49:25 -0300

    Add CRUD from Temas

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,26 +1,39 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
+
+    const navigate = useNavigate();
+
+    const { handleLogout } = useContext(AuthContext)
+
+    function logout() {
+
+        handleLogout()
+        alert('O Usuário foi desconectado com sucesso!')
+        navigate('/')
+    }
+    
     return (
         <>
-        <div className="w-full bg-indigo-900 text-white
-        flex justofy-center py-4">
+            <div className='w-full bg-indigo-900 text-white
+                flex justify-center py-4'>
 
-            <div className="container flez justify-between text-lg">
-                <Link to="/home"className="text-2x1 font-bold">Blog Pessoal</Link>
+                <div className="container flex justify-between text-lg">
+                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
-                <div className="flex gap-4">
-                    Postagens
-                    Temas 
-                    Cadastrar Temas 
-                    Perfil 
-                    Sair 
+                    <div className='flex gap-4'>
+                        Postagens
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
+                        Perfil
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+                    </div>
                 </div>
             </div>
-
-        </div>
         </>
     )
 }
 
-export default Navbar;
+export default Navbar
